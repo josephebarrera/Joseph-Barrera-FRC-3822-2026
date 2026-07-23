50a0e1fb47f790905e8c0ed949b3a98655368f35
Joseph Barrera <josephebarrera@gmail.com>
2026-07-23 08:17:39 -0400

    Fix joystick drive direction: negate stick axes to match WPILib forward convention, enable alliance-relative control

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -37,12 +37,12 @@ public class RobotContainer
     */
     public SwerveInputStream driveInputStream = SwerveInputStream.of(
     drivebase.getSwerveDrive(),
-    () -> driverXbox.getLeftY(),
-    () -> driverXbox.getLeftX())
+    () -> -driverXbox.getLeftY(),
+    () -> -driverXbox.getLeftX())
     .withControllerRotationAxis(()-> driverXbox.getRightX())
     .deadband(Constants.OperatorConstants.DEADBAND)
-    .scaleTranslation(0.5) //Originally 0.5 
-    .allianceRelativeControl(false);
+    .scaleTranslation(0.5) //Originally 0.5
+    .allianceRelativeControl(true);
       
     /**
     * The container for the robot. Contains subsystems, OI devices, and commands.
